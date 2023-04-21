820f131035ad262a5a606fd7bb2b768a8566a187
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-04-21 16:34:13 +0300

    Imlement CookModelTest in test_models.py

---
+++ kitchen/tests/test_models.py
@@ -0,0 +1,51 @@
+from django.test import TestCase
+
+from kitchen.models import Cook
+
+
+class CookModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Cook.objects.create(
+            username="test",
+            password="test12345",
+            years_of_experience=5,
+            first_name="TestFirstName",
+            last_name="TestLastName")
+
+    def test_username_label(self):
+        cook = Cook.objects.get(id=1)
+        field_label = cook._meta.get_field("username").verbose_name
+        self.assertEqual(field_label, "username")
+
+    def test_password_label(self):
+        cook = Cook.objects.get(id=1)
+        field_label = cook._meta.get_field("password").verbose_name
+        self.assertEqual(field_label, "password")
+
+    def test_years_of_experience_label(self):
+        cook = Cook.objects.get(id=1)
+        field_label = cook._meta.get_field("years_of_experience").verbose_name
+        self.assertEqual(field_label, "years of experience")
+
+    def test_first_name_label(self):
+        cook = Cook.objects.get(id=1)
+        field_label = cook._meta.get_field("first_name").verbose_name
+        self.assertEqual(field_label, "first name")
+
+    def test_last_name_label(self):
+        cook = Cook.objects.get(id=1)
+        field_label = cook._meta.get_field("last_name").verbose_name
+        self.assertEqual(field_label, "last name")
+
+    def test_cook_str(self):
+        cook = Cook.objects.get(id=1)
+        expected_object_name = f"{cook.username} " \
+                               f"({cook.first_name} {cook.last_name})"
+        self.assertEqual(str(cook), expected_object_name)
+
+    def test_get_absolute_url(self):
+        cook = Cook.objects.get(id=1)
+        # This will also fail if the urlconf is not defined.
+        self.assertEqual(cook.get_absolute_url(), "/cooks/1/")
+
