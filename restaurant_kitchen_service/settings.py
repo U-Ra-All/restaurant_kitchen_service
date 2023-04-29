67251166221e588f5b01551d0d0e609be5af1010
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-04-29 08:29:35 +0300

    Set DJANGO_DEBUG to False in .env

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', "") != "False"
+DEBUG = str(os.getenv('DJANGO_DEBUG')) != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
